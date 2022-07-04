b9d554389110ba4d58f4e287d10623f9e2484617
atn95 <antannguyen2000@gmail.com>
2022-07-04 14:35:07 -0500

    Displayed board

--- tetris.js
+++ tetris.js
@@ -1,5 +1,57 @@
-const gameboard = document.querySelector(`#gameboard`);
+const gameboard = document.querySelector(`#gameboard`); //canvas is 808x800
 const c = gameboard.getContext('2d');
+gameboard.width = 800;
+gameboard.height = 600;
+const sqSide = 25;
 
-c.fillStyle = `black`;
-c.fillRect(0, 0, 800, 800);
+//each square will be 50x50 12 across = 600px
+/*Start drawing at 100px and end 100px from edge*/
+const board = [
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
+  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+];
+
+function draw() {
+  requestAnimationFrame(draw);
+  console.log(` width: ` + gameboard.width + ` height: ` + gameboard.height);
+  c.fillStyle = `black`;
+  c.fillRect(0, 0, 800, 800); //draw black background
+  /*double loop to go through the 2D array*/
+  const startX = (gameboard.width - board[0].length * sqSide) / 2;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] == 10) {
+        c.fillStyle = `white`;
+        c.fillRect(
+          startX + j * sqSide,
+          20 + i * sqSide,
+          sqSide - 2,
+          sqSide - 2
+        );
+      }
+    }
+  }
+}
+
+//start animation calls
+draw();
